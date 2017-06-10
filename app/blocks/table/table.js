ec9f2280be90bc72cc32edfa539223842fb1e520
Alexey Bogoslovskiy <bogoslovskiya@gmail.com>
2017-06-10 13:02:12 +0300

    toggle table

--- app/blocks/table/table.js
+++ app/blocks/table/table.js
@@ -7,6 +7,7 @@ export default () => {
     return;
   }
 
+  // checkboxes
   table.on('change', '.checkbox__input', function (e, data) { // eslint-disable-line func-names, consistent-return
     const checkbox = $(this);
     const row = checkbox.parents('.table__row_main');
@@ -15,49 +16,51 @@ export default () => {
     row.toggleClass('table__row_selected', checkbox.prop('checked'));
 
     // if the row is the first one, then check/uncheck all children checkboxes
-    if (row.index() === 0 && !checkbox.prop('indeterminate')) {
-      return row // eslint-disable-line consistent-return
-        .nextAll('.table__row_main')
+    const isFirstParentCheckbox = row
+      .parent()
+      .parent()
+      .hasClass('table');
+
+    if (isFirstParentCheckbox) {
+      row
+        .parent()
+        .find('.table__row_children')
         .find('.checkbox__input')
         .prop('checked', checkbox.prop('checked'))
         .trigger('change', ['fromParent']);
     }
 
-    // make no sense to check childrens state, if state was changed on parent checkbox
+    // make no sense to check childrens state, if it was changed on parent checkbox
     if (data && data === 'fromParent') {
       return; // eslint-disable-line consistent-return
     }
 
-    // check childrens state
-    const parentRow = row.parents('.table__row_parent');
-    const parentCheckbox = parentRow
-      .find('.table__row_main')
+    // if childrens are selected, then mark parent as checked
+    // if childrens aren't selected, then mark parent as unchecked
+    // otherwise mark parent as indeterminate
+    console.log('children has been changed');
+  });
+
+  // toggle row
+  table.on('click', '.table__toggle', function (e) { // eslint-disable-line func-names
+    e.preventDefault();
+    e.stopPropagation();
+
+    const btn = $(this);
+    const isActive = btn.hasClass('table__toggle_active');
+    const childrenRow = btn
+      .parents('.table__row_parent')
       .eq(0)
-      .find('.checkbox__input');
+      .children('.table__row_children');
 
-    const childCheckboxes = parentRow.find('.checkbox__input');
-    const countChecked = [...childCheckboxes].filter(el => el.checked).length;
-
-    // if all are checked, then check parent as well
-    // TODO need to exclude parent froms states before
-    // if (countChecked === childCheckboxes.length) {
-    //   parentCheckbox
-    //     .prop('checked', true)
-    //     .trigger('checked', ['fromParent']);
-    // }
-    // if all aren't checked, then unchech parent as well
-    // TODO need to exclude parent froms states before
-    // if (!countChecked) {
-    //   parentCheckbox
-    //     .prop('checked', false)
-    //     .trigger('checked', ['fromParent']);
-    // }
-    // otherwise, mark parent as indeterminate
-    // TODO rereplace that if, on else..
-    if (countChecked && countChecked < childCheckboxes.length) {
-      parentCheckbox
-        .prop({ indeterminate: true })
-        .trigger('change', ['fromParent']);
+    if (isActive) {
+      childrenRow.slideUp(250, () => {
+        btn.removeClass('table__toggle_active');
+      });
+    } else {
+      childrenRow.slideDown(250, () => {
+        btn.addClass('table__toggle_active');
+      });
     }
   });
 };
