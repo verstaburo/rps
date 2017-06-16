96118faa097544f08a995ea108123d0140fef0c8
Alexey Bogoslovskiy <bogoslovskiya@gmail.com>
2017-06-16 11:34:58 +0300

    edit-card-data-popup logic

--- app/components/edit-card-data-popup/edit-card-data-popup.js
+++ app/components/edit-card-data-popup/edit-card-data-popup.js
@@ -7,20 +7,79 @@ export default () => {
     return;
   }
 
+  // remove row
   block.on('click', '.js-edit-card-data-popup-remove', function (e) { // eslint-disable-line func-names
     e.preventDefault();
-    const row = $(this).parents('.default-table__row');
+    const row = $(this).parents('.edit-card-data-popup__row');
 
     if (!row.length) {
       return;
     }
 
+    // PUT THIS CODE IN AJAX SUCCESS CAL;BACL
     row.fadeOut(250, row.remove);
   });
 
+  // add row
   block.on('click', '.js-edit-card-data-popup__add-row', (e) => {
     e.preventDefault();
 
-    // logic goes here
+    const lastRow = block
+      .find('.edit-card-data-popup__row')
+      .last();
+
+    const clone = lastRow
+      .clone(true, true)
+      .hide()
+      .insertAfter(lastRow);
+
+    clone
+      .find('.select')
+      .each((index, el) => {
+        el.selectedIndex = 0; // eslint-disable-line no-param-reassign
+        $(el).trigger('change');
+      });
+
+    clone.fadeIn();
   });
+
+  // activate other params, if first one selected
+  const toggleParams = function () { // eslint-disable-line func-names
+    const el = $(this);
+    const selected = el.find(':selected');
+    const childrenCells = el
+      .parents('.edit-card-data-popup__cell')
+      .nextAll();
+
+    if (selected.prop('disabled')) {
+      // hide select, show 'select param text'
+      childrenCells.each(function () { // eslint-disable-line func-names
+        const childCell = $(this);
+
+        childCell
+          .find('.select')
+          .hide()
+          .parent()
+          .find('.edit-card-data-popup__select-param')
+          .fadeIn();
+      });
+    } else {
+      // show select, hide 'select param text'
+      childrenCells.each(function () { // eslint-disable-line func-names
+        const childCell = $(this);
+
+        childCell
+          .find('.edit-card-data-popup__select-param')
+          .hide()
+          .parent()
+          .find('.select')
+          .fadeIn();
+      });
+    }
+  };
+
+  block
+    .on('change', '.js-edit-card-data-popup__parent-select', toggleParams)
+    .find('.js-edit-card-data-popup__parent-select')
+    .each(toggleParams);
 };
