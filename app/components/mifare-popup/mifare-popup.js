6c8dc78c038666b4b3ac4e7e82ebd19e1a06029c
Alexey Bogoslovskiy <bogoslovskiya@gmail.com>
2017-06-19 15:43:45 +0300

    block/unblock mifare card

--- app/components/mifare-popup/mifare-popup.js
+++ app/components/mifare-popup/mifare-popup.js
@@ -7,4 +7,27 @@ export default () => {
   }
 
   block.tabs();
+
+  // block/unblock
+  block.on('click', '.mifare-popup__action', function (e) { // eslint-disable-line func-names
+    e.preventDefault();
+    const el = $(this);
+
+    // block
+    if (el.hasClass('mifare-popup__action_block')) {
+      // the code should be in ajax success callback
+      block
+        .find('.mifare-popup__alert')
+        .slideDown(250, () => {
+          block.addClass('mifare-popup_blocked');
+        });
+    } else {
+      // the code should be in ajax success callback
+      block
+        .find('.mifare-popup__alert')
+        .slideUp(250, () => {
+          block.removeClass('mifare-popup_blocked');
+        });
+    }
+  });
 };
