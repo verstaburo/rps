e45b7deb52892d49456c05d5d114b23fd338d795
Alexey Bogoslovskiy <bogoslovskiya@gmail.com>
2017-06-22 08:25:18 +0300

    fixes

--- app/components/clients-popup/clients-popup.js
+++ app/components/clients-popup/clients-popup.js
@@ -9,10 +9,49 @@ export default () => {
 
   block.tabs();
 
-  // set active tab, depends on 'data-subtarget' param
+
   const popup = $('#clients-popup');
 
   popup.on('popup:show', (e, subtarget) => {
+    // scroll settings
+    const vH = $(window).height();
+    const header = popup.find('.popup__header');
+    const sidebar = popup.find('.clients-popup__sidebar');
+    const sidebarGroup = sidebar.find('.inner-sidebar__group');
+    const sidebarBtn = sidebar.find('.clients-popup__sidebar-btn');
+
+    // sidebar
+    sidebar.height(vH - header.outerHeight());
+    sidebarGroup.height(vH - header.outerHeight() - sidebarBtn.outerHeight());
+
+    // common-clients-info
+    {
+      const tab = popup.find('.clients-common-info');
+      const content = tab.find('.clients-common-info__content');
+      const btn = tab.find('.clients-common-info__save-button');
+
+      content.height(vH - header.outerHeight() - btn.outerHeight());
+    }
+
+    // individual-info
+    {
+      const tab = popup.find('.individual-info');
+      const actions = tab.find('.search-bar-add');
+      const content = tab.find('.accordion');
+
+      content.height(vH - header.outerHeight() - actions.outerHeight());
+    }
+
+    // ts-list
+    {
+      const tab = popup.find('.clients-popup__ts-list');
+      const actions = tab.find('.search-bar-add');
+      const content = tab.find('.ts-list');
+      console.log(tab, actions, content);
+      content.height(vH - header.outerHeight() - actions.outerHeight());
+    }
+
+    // set active tab, depends on 'data-subtarget' param
     if (!subtarget) {
       return;
     }
