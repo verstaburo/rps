4536c2f789b2faaf97bccc357eb193ad6c556d01
Alexey Bogoslovskiy <bogoslovskiya@gmail.com>
2017-06-27 12:38:24 +0300

    fix control-center

--- app/pages/control-center/control-center.js
+++ app/pages/control-center/control-center.js
@@ -10,14 +10,19 @@ export default function controlCenter() {
   const vH = $(window).height();
   const header = $('.header');
   const sidebar = block.find('.control-center__sidebar');
-  const content = block.find('.control-center__content');
+  const container = block.find('.control-center__container');
+  const table = block.find('.control-center__table');
 
   const heights = vH - header.outerHeight();
 
   // sidebar
   sidebar.height(heights);
   // content
-  content.height(heights);
+  table.height(heights - container.outerHeight());
 }
+
+$('.control-center .accordion').on('click', () => {
+  setTimeout(controlCenter, 300);
+});
 
 $(window).on('resize', controlCenter);
